4e3ddf86b04a1941e2607a0f95c1fbdb6283b192
dennis wang <dwang0816@gmail.com>
2019-06-09 17:01:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def line(places)
+  if place.empty?
+    puts "The line is currently empty."
+  else
+    deli_line = []
+    places.each.with_index(1) do |place, index|
+    deli_line.push("#{index}. #{place}")
+  end
+  puts "The line is currently: #{deli_line.join("")}"
+  end
+end
